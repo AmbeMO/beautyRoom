cca96cffc75aa6f0931dcbc5b622bb596eb2ea2b
AmbeMO <milamo@mail.ua>
2020-12-09 18:03:58 +0200

    + scroll menu

--- project/js/script.js
+++ project/js/script.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
 	$('.header-menu__burger').click(function(event){
 		$('.header-menu__burger,.header-menu__menu').toggleClass('active');
-		$('body').toggleClass('lock');// блокірує прокрутку коли ми в медія меню
+		$('body').toggleClass('lock');
 	}); 
+	
+	$('.header-menu__list li a').on('click' ,function(e){
+		e.preventDefault();
+		if ($(window).width() < 767) {
+			$('.header-menu__burger,.header-menu__menu').removeClass('active');
+			$('body').removeClass("lock");
+			$('html').animate({ 
+	    	    scrollTop: $("." + $(this).attr("data-block")).offset().top  - 50
+	        }, 500);
+		}  else {
+			$('html').animate({ 
+	    	    scrollTop: $("." + $(this).attr("data-block")).offset().top  - 80
+	        }, 500);
+		}
+	});
 });
